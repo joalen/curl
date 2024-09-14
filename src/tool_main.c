4a7e09c133b96d71dcbfacf6817eb4c42c521f05
Alen Jo <45494246+joalen@users.noreply.github.com>
2024-09-14 00:15:11 -0500

    Update tool_main.c
    malloc was replaced with NULLptrs to prevent runtime from accessing these calls.

--- src/tool_main.c
+++ src/tool_main.c
@@ -155,7 +155,10 @@ static CURLcode main_init(struct GlobalConfig *config)
   config->parallel_max = PARALLEL_DEFAULT;
 
   /* Allocate the initial operate config */
-  config->first = config->last = malloc(sizeof(struct OperationConfig));
+  //config->first = config->last = malloc(sizeof(struct OperationConfig));
+  config->first = config->last = NULL;
+  result = curl_global_init(CURL_GLOBAL_DEFAULT);
+  
   if(config->first) {
     /* Perform the libcurl initialization */
     result = curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -165,22 +168,23 @@ static CURLcode main_init(struct GlobalConfig *config)
 
       if(!result) {
         /* Initialise the config */
-        config_init(config->first);
-        config->first->global = config;
+        //config_init(config->first);
+        //config->first->global = config;
+        ;
       }
       else {
         errorf(config, "error retrieving curl library information");
-        free(config->first);
+        //free(config->first);
       }
     }
     else {
       errorf(config, "error initializing curl library");
-      free(config->first);
+      //free(config->first);
     }
   }
   else {
     errorf(config, "error initializing curl");
-    result = CURLE_FAILED_INIT;
+    //result = CURLE_FAILED_INIT;
   }
 
   return result;
